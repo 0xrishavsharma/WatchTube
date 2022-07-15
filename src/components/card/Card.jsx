a9a7c0a1a09743db7ab640627d39a84eae7bb085
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-07-16 01:27:26 +0530

    Fourth

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import styled from 'styled-components';
+import Thumbnail1 from "../../assets/img/thumbnails/thumbnail1.jpg"
 
 const Container = styled.div`
-    color: ${({theme}) => theme.text };
+    color: ${({ theme }) => theme.text };
+    width: 360px;
+    margin-bottom: 30px;
+    cursor: pointer;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 202px;
+    background-color: #999;
+
 `;
 
 
 const Card = () => {
   return (
     <Container>
-        Card
+        <Image src={Thumbnail1} />
     </Container>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,55 +2,14 @@ import styled from "styled-components"
 import Card from "../components/card/Card";
 
 const Container = styled.div`
-    
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 `;
 
 const Home = () => {
   return (
     <Container>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
         <Card />
         <Card />
         <Card />
