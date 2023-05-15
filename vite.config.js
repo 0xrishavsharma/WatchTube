50d4208ec1dcaa86c2ba3f9836c20a2d252d23ca
Rishav Sharma <rishavtosharma@gmail.com>
2023-05-15 23:53:11 +0530

    Trying to make deployed app work: 1

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8800/',
+        target: 'https://watchtube-backend.onrender.com/',
         changeOrigin: true,
         secure: false,
         ws: true
